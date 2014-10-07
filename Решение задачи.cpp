f8d926fec4bd23041f0472712694151e62d55778
DDHunter13 <1310nero@mail.ru>
2014-10-07 20:54:54 +0400

    Update Решение задачи.cpp

--- Решение задачи.cpp
+++ Решение задачи.cpp
@@ -2,6 +2,7 @@
 #include <math.h>
 #include <locale.h>
 #include <iomanip>
+#include <algorithm>
 using namespace std;
 
 int main(void) {
@@ -11,16 +12,16 @@ int main(void) {
 	int const n = 10;
 	int const n2 = n / 2;
 	double B[n2], C[n2], A[n];
-	int i, j, i1, j2, k, err;
-	double m1, m2, r, d;
+	int i, i1, k, err;
+	double r, d;
 	int D[n];
 
 	for (i = 0; i < n; ++i) {
 		D[i] = 0;
 	}
 
-	cout << "Введите " <<n<< " элементов массива" << endl;
-	for (int i = 0; i < n; ++i) {
+	cout << "Введите " << n << " элементов массива" << endl;
+	for (i = 0; i < n; ++i) {
 		err = scanf_s("%lf", &d);
 		if (err != 1) {
 			cout << "Введенны неверные данные" << endl;
@@ -29,49 +30,38 @@ int main(void) {
 		A[i] = d;
 	}
 
-	for (k = 0; k < n / 2; ++k) {
-		for (i = 0; i < n; ++i) {
-			for (j = i + 1; j < n; ++j) {
-				if ((D[i] == 0) && (D[j] == 0)) {
-					r = abs(A[i] - A[j]);
-					m1 = A[i];
+	sort(A, A + 10);
+	cout << endl;
+
+	for (k = 0; k < n2; ++k) {
+		for (i = 0; i < n - 1; ++i) {
+			if ((D[i] == 0) && (D[i + 1] == 0)) {
+				r = abs(A[i] - A[i + 1]);
+				i1 = i;
+				break;
+			}
+		}
+		for (i; i < n - 1; ++i) {
+			if ((D[i] == 0) && (D[i + 1] == 0)) {
+				if (r > abs(A[i] - A[i + 1])) {
+					r = abs(A[i] - A[i + 1]);
 					i1 = i;
-					m2 = A[j];
-					j2 = j;
-					goto l1;
 				}
 			}
 		}
-		l1:
-		for (i; i < n; ++i){
-			for (j = i + 1; j < n; ++j){
-				if ((D[i] == 0) && (D[j] == 0)) {
-					if (r > abs(A[i] - A[j])) {
-						r = abs(A[i] - A[j]);
-						m1 = A[i];
-						i1 = i;
-						m2 = A[j];
-						j2 = j;
-					}
-				}
-			}
-		}
-		if (m1 > m2) {
-			swap(m1, m2);
-		}
-		r = 0;
 		D[i1] = 1;
-		D[j2] = 1;
-		B[k] = m1;
-		C[k] = m2;
+		D[i1 + 1] = 1;
+		B[k] = A[i1];
+		C[k] = A[i1 + 1];
+
 	}
 
 	for (i = 0; i < n2; ++i){
-		cout << B[i] << " " ;
+		cout << B[i] << " ";
 	}
 	cout << endl;
 	for (i = 0; i < n2; ++i){
-		cout << C[i] << " " ;
+		cout << C[i] << " ";
 	}
 
 	return 0;
